2edd6bf89d16b7c668764a3ca2847bebaffaf407
h581296 <581296@stud.hvl.no>
2024-09-30 09:39:45 +0200

    a

--- dat103-h24-bash/test.sh
+++ dat103-h24-bash/test.sh
@@ -3,13 +3,13 @@ mkdir task1-bash
 cp jurassicParkCast.txt task1-bash/cast.txt
 ls task1-bash
 echo '---------------------------------------------------'
-tail -n +2 task1-bash cast.txt | sort -t, -k4
+tail -n +2 task1-bash/cast.txt | sort -t, -k4,4n
 echo '---------------------------------------------------'
 grep "^L" task1-bash/cast.txt
 echo '---------------------------------------------------'
-grep -i 'women' task1-bash/cast.txt > task1-bash/women.txt
+awk -F, '$4 ~ /F/' task1-bash/cast.txt | tee task1-bash/women.txt
 echo '---------------------------------------------------'
-grep i 'men' task1-bash/cast.txt > task1-bash/men.txt
+awk -F, '$4 ~ /M/' task1-bash/cast.txt | tee task1-bash/men.txt
 echo '---------------------------------------------------'
 for file in task1-bash/*.txt; do
   cp "$file" "${file%.txt}-1.txt"
