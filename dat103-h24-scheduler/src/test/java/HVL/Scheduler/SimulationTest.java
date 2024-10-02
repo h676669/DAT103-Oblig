fdabd307f3c92af0bee461787ca0248a62271ded
h676669 <142335301+h676669@users.noreply.github.com>
2024-10-02 13:17:06 +0200

    hei

--- dat103-h24-scheduler/src/test/java/HVL/Scheduler/SimulationTest.java
+++ dat103-h24-scheduler/src/test/java/HVL/Scheduler/SimulationTest.java
@@ -60,39 +60,39 @@ public class SimulationTest {
         // Expected output after 33 time quanta
         assertThat(steps, contains(
                 "T=0 Scheduled: T1 Ready: T2, T3",
-                "T=1 Scheduled: T2 Ready: T3",
+                "T=1 Scheduled: T2 Ready: T3", //t1 finished
                 "T=2 Scheduled: T2 Ready: T3",
                 "T=3 Scheduled: T2 Ready: T3",
                 "T=4 Scheduled: T2 Ready: T3, T4, T5, T6",
-                "T=5 Scheduled: T3 Ready: T4, T5, T6, T2",
+                "T=5 Scheduled: T3 Ready: T4, T5, T6, T2", //t2 back in queue (1)
                 "T=6 Scheduled: T3 Ready: T4, T5, T6, T2",
                 "T=7 Scheduled: T3 Ready: T4, T5, T6, T2",
-                "T=8 Scheduled: T3 Ready: T4, T5, T6",
-                "T=9 Scheduled: T3 Ready: T4, T5, T6",
-                "T=10 Scheduled: T3 Ready: T4, T5, T6",
-                "T=11 Scheduled: T3 Ready: T4, T5, T6",
-                "T=12 Scheduled: T4 Ready: T5, T6",
-                "T=13 Scheduled: T4 Ready: T5, T6",
-                "T=14 Scheduled: T5 Ready: T6, T8, T7",
-                "T=15 Scheduled: T5 Ready: T6, T8, T7",
-                "T=16 Scheduled: T5 Ready: T6, T8, T7, T9, T10",
-                "T=17 Scheduled: T6 Ready: T8, T7, T9, T10",
-                "T=18 Scheduled: T6 Ready: T8, T7, T9, T10",
-                "T=19 Scheduled: T6 Ready: T8, T7, T9, T10",
-                "T=20 Scheduled: T6 Ready: T8, T7, T9, T10",
-                "T=21 Scheduled: T7 Ready: T8, T9, T10",
-                "T=22 Scheduled: T7 Ready: T8, T9, T10",
-                "T=23 Scheduled: T7 Ready: T8, T9, T10",
-                "T=24 Scheduled: T7 Ready: T8, T9, T10",
-                "T=25 Scheduled: T8 Ready: T9, T10",
-                "T=26 Scheduled: T8 Ready: T9, T10",
-                "T=27 Scheduled: T9 Ready: T10",
-                "T=28 Scheduled: T9 Ready: T10",
-                "T=29 Scheduled: T9 Ready: T10",
-                "T=30 Scheduled: T10 Ready: ",
-                "T=31 Scheduled: T10 Ready: ",
-                "T=32 Scheduled: T10 Ready: ",
-                "T=33 Scheduled: T10 Ready: "
+                "T=8 Scheduled: T4 Ready: T5, T6, T2", //t3 finished
+                "T=9 Scheduled: T4 Ready: T5, T6, T2",
+                "T=10 Scheduled: T5 Ready: T6, T2", //t4 finished
+                "T=11 Scheduled: T5 Ready: T6, T2",
+                "T=12 Scheduled: T5 Ready: T6, T2",
+                "T=13 Scheduled: T5 Ready: T6, T2",
+                "T=14 Scheduled: T6 Ready: T2, T7, T8", //t5 finished
+                "T=15 Scheduled: T6 Ready: T2, T7, T8",
+                "T=16 Scheduled: T6 Ready: T2, T7, T8, T9, T10",
+                "T=17 Scheduled: T6 Ready: T2, T7, T8, T9, T10",
+                "T=18 Scheduled: T2 Ready: T7, T8, T9, T10, T6", //t6 back in queue (2)
+                "T=19 Scheduled: T7 Ready: T8, T9, T10, T6", //t2 finished
+                "T=20 Scheduled: T7 Ready: T8, T9, T10, T6",
+                "T=21 Scheduled: T7 Ready: T8, T9, T10, T6",
+                "T=22 Scheduled: T7 Ready: T8, T9, T10, T6",
+                "T=23 Scheduled: T8 Ready: T9, T10, T6, T7", //t7 back in queue (1)
+                "T=24 Scheduled: T8 Ready: T9, T10, T6, T7",
+                "T=25 Scheduled: T9 Ready: T10, T6, T7", //t8 finished
+                "T=26 Scheduled: T10 Ready: T6, T7", //t9 finished
+                "T=27 Scheduled: T10 Ready: T6, T7",
+                "T=28 Scheduled: T10 Ready: T6, T7",
+                "T=29 Scheduled: T10 Ready: T6, T7",
+                "T=30 Scheduled: T6 Ready: T7", //t10 finished
+                "T=31 Scheduled: T6 Ready: T7",
+                "T=32 Scheduled: T7 Ready: ", //t6 finished
+                "T=33 Scheduled: Ready: " //t7 finished
         ));
     }
 
